eb5863a09527a56faf765c44bf9348d9cd833f58
csteinmann <cle.s@t-online.de>
2023-09-08 23:05:38 +0200

    Before integrating rotation in index.html

--- bookrec/admin.py
+++ bookrec/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from bookrec.models import Book, Survey, Question, Choice, Submission
 
 
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title_without_series']
+
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Survey)
 admin.site.register(Question)
 admin.site.register(Choice)
--- bookrec/views.py
+++ bookrec/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from bookrec.models import Book, Survey
 from bookrec.forms import SurveyForm
 import csv
 import os
 from pathlib import Path
+import pickle
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -12,12 +13,54 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Create your views here.
 
 def index(request):
-    # Create context_dict using our helper function
-    context_dict = {'books_ii': read_csv_recs(os.path.join(BASE_DIR, 'data/recs_ii.csv')),
-                    'books_uu': read_csv_recs(os.path.join(BASE_DIR, 'data/recs_uu.csv')),
-                    'books_als': read_csv_recs(os.path.join(BASE_DIR, 'data/recs_als.csv'))}
 
-    return render(request, 'bookrec/index.html', context_dict)
+    # Create entries for the book list using our helper function
+    books_ii = read_csv_recs(os.path.join(BASE_DIR, 'data/recs_ii.csv'))
+    books_uu = read_csv_recs(os.path.join(BASE_DIR, 'data/recs_uu.csv'))
+    books_als = read_csv_recs(os.path.join(BASE_DIR, 'data/recs_als.csv'))
+
+    book_list = [books_ii, books_uu, books_als]
+
+    # open pickle to receive current rotation state
+    with open('rotator_pickle.pk', 'rb') as fi:
+        current_rotation_list = pickle.load(fi)
+        rotation_state = current_rotation_list[0]
+
+    # search current rotation state and create content dictionary accordingly
+    # also call the function to initiate the rotation of the state list
+    if rotation_state == 'IIUU':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[0], 'books_b': book_list[1]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+    elif rotation_state == 'IIALS':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[0], 'books_b': book_list[2]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+    elif rotation_state == 'UUALS':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[1], 'books_b': book_list[2]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+    elif rotation_state == 'UUII':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[1], 'books_b': book_list[0]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+    elif rotation_state == 'ALSII':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[2], 'books_b': book_list[0]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+    elif rotation_state == 'ALSUU':
+        context_dict = {'rotation_state': rotation_state, 'books_a': book_list[2], 'books_b': book_list[1]}
+        rotate_content_list()
+        return render(request, 'bookrec/index.html', context_dict)
+
+
+def rotate_content_list():
+    with open('rotator_pickle.pk', 'rb') as fi:
+        current_rotation_list = pickle.load(fi)
+    current_rotation_list.append(current_rotation_list[0])
+    del current_rotation_list[0]
+    with open('rotator_pickle.pk', 'wb') as fi:
+        pickle.dump(current_rotation_list, fi)
 
 
 def read_csv_recs(path):
