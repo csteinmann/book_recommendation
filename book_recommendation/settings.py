8f5fd2e536671ef0aed5df25e7edbc45bd252cc4
csteinmann <cle.s@t-online.de>
2023-09-28 18:31:47 +0200

    Changing name of Logger

--- book_recommendation/settings.py
+++ book_recommendation/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-3vxd%ge*y_o$9lmljc6xc%jxi^3-%e9(81*ep_ky%ts5qpam%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -138,13 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
