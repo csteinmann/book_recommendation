4e11c5308edf7ba9bc32273c6ffa2ab44fa3e579
csteinmann <61979000+csteinmann@users.noreply.github.com>
2023-10-07 17:47:45 +0200

    Update settings.py

--- book_recommendation/settings.py
+++ book_recommendation/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bookrec-3nre.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = [''bookrec-3nre.onrender.com', '127.0.0.1']
 
 
 # Application definition
